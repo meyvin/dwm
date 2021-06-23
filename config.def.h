c58553cd1e8e627ade447c11f2756e9d1cb688c2
Meyvin <meyvin@mailbox.org>
2021-06-23 16:22:34 +0200

    nord colors

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "5%-", "unm
 static const char *prtscrcmd[] = { "flameshot", "gui", NULL};
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const int gappx     = 10;                 /* gaps between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const int gappx     = 6;                 /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -21,24 +21,18 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]	    = {"Jetbrainsmono Nerd Font:size=11:antialias=true:autohint=true",
-                                       "Hack:size=8:antialias=true:autohint=true",
-                                       "JoyPixels:size=10:antialias=true:autohint=true"};
-static const char dmenufont[]       = "Mononoki Nerd Font:size=11";
-static const char col_offwhite[]    = "#f8f8f2";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_purple[]      = "#bd93f9";
-static const char col_pink[]        = "#7f0049";
-static const char col_black[]       = "#282a36";
-static const char col_grey[]        = "#44475a";
+static const char *fonts[]	        = {"Jetbrainsmono Nerd Font:size=11:antialias=true:autohint=true"};
+static const char dmenufont[]       = "Jetbrainsmono Nerd Font:size=11:antialias=true:autohint=true";
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#434c5e";
+static const char col_gray3[]       = "#d8dee9";
+static const char col_gray4[]       = "#eceff4";
+static const char col_cyan[]        = "#88c0d0";
+static const char col_polar[]       = "#4c566a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeSel] = { col_offwhite, col_grey, col_pink },
-	[SchemeNorm]  = { col_gray3, col_black,  col_black  },
+  [SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+  [SchemeSel]  = { col_gray4, col_polar,  col_cyan  },
 };
 
 /* tagging */
@@ -85,7 +79,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_grey, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_polar, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *filemanager[] = { "pcmanfm", NULL };
 static const char *browser[] = { "brave", NULL };
